adf316d47ef311deffc38c591dccb8d5acaecf94
SessCoffi <sess.coffi@gmail.com>
2024-11-25 18:00:51 +0000

    changer h1 dans App.tsx

--- burgerhouse/src/App.tsx
+++ burgerhouse/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-2xl text-blue-500">Burger Houses</h1>
+      <h1 className="text-2xl text-blue-500">Burger Houses le meilleur</h1>
     </div>
   );
 }
